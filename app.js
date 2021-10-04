75234be92f37094d5f431beb467057756a90c740
Kevin Do <kevind0501@gmail.com>
2021-10-04 15:44:02 -0700

    added HTML CSS JS - working submit button and reset

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
 // import functions and grab DOM elements
+const inputNumber = document.getElementById('input-number');
+const submitButton = document.getElementById('submit-button');
+const resultText = document.getElementById('result-text');
+const guessesLeft = document.getElementById('guesses-left');
+const restartButton = document.getElementById('restart-button');
 
 // initialize global state
+let numGuessesLeft = 4;
+const randomNumber = Math.floor(Math.random() * 20) + 1;
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+submitButton.addEventListener('click', ()=> {
+    const guess = Number(inputNumber.value);
+    numGuessesLeft--;
+    let userMessage;
+    if(guess === randomNumber) {
+        userMessage = 'You win.. for now';
+    } else if (numGuessesLeft === 0) {
+        userMessage = 'Out of guesses.. goodbye';
+        submitButton.style.display = 'none';
+        restartButton.style.display = 'block';
+    } else if (guess > randomNumber) {
+        userMessage = 'Number too high';
+    } else if (guess < randomNumber) {
+        userMessage = 'Number too low';
+    }
+    resultText.textContent = `${userMessage}`;
+    guessesLeft.textContent = `You have ${numGuessesLeft} guesses left.`
+
+    console.log(guessesLeft)
+});
+
+restartButton.addEventListener('click', ()=> {
+    numGuessesLeft = 4;
+    resultText.textContent = ':)';
+    guessesLeft.textContent = `You have ${numGuessesLeft} guesses left.`
+});
+

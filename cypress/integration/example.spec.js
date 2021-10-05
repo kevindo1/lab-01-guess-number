1c057b6d786e54afdc7752638756960875852556
Kevin Do <kevind0501@gmail.com>
2021-10-05 10:59:37 -0700

    added cypress test

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -1,12 +1,20 @@
 
-// describe('Example Test', ()=>{
-//     before(()=>{
-//         cy.visit('/');
-//     });
+describe('Example Test', ()=>{
+    before(()=>{
+        cy.visit('/');
+    });
 
-//     describe('HTML elements', ()=>{   
-//         it('contains the alchemy logo', ()=>{
-//             // cy.get('.main-logo').should('be.visible');
-//         });
-//     });
-// });
+    describe('HTML elements', ()=>{   
+        it('contain logo image', ()=>{
+            cy.get('#logo').should('be.visible');
+        });
+    });
+
+    describe('user interaction', ()=>{
+        it('should submit the guess number and compare it to the random number', ()=> {        
+            cy.get('#submit-button').click();
+            cy.get('#result-text').should('be.visible');
+            // cy.get('#result-text').should('contain.text', 'Number too low');
+        });
+    });
+});
